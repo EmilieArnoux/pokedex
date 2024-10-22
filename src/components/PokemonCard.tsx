0a9dd828ebd5195544fa1c989a666120e6bd2e46
Emilie Arnoux <arnoux.emilie@live.fr>
2024-10-22 17:01:28 +0200

    ajout des données dans un composant

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,25 @@
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
     <figure> 
-        <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt = "Bulbasaur"/>
-        <figcaption>Bulbasaur</figcaption>
+        <div>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+        </div>
+        <figcaption>{pokemon.name}</figcaption>
      </figure>
      );
   }
-  
+
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+   
   export default PokemonCard;
